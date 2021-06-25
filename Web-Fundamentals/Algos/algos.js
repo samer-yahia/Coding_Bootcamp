25fe361af17d2a8ebdb241fbfb8d7138d8b1030c
Samer Yahia <syahia@uci.edu>
2021-06-25 16:35:36 -0700

    End of Week 1 Bootcamp !!! :D

---
+++ Web-Fundamentals/Algos/algos.js
@@ -0,0 +1,24 @@
+// var countPositives = 0;
+// var numbers = [3, 4, -2, 7, 16, -8, 0];
+    
+// for(var i=0; i < numbers.length; i++){
+//     if(numbers[i] >= 0){
+//         countPositives++;
+//     }
+// }
+    
+// console.log("there are " + countPositives + " positive values");
+
+
+// var array = ["e", "d", "c", "b", "a"];
+
+// function Reverse(array){
+//     var = temp[];
+// for(var i = 0)
+//     console.log(i)
+// }
+
+
+
+
+
